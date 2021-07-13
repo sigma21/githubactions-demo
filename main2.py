61dc8488ef501966f721845ef55b9717b21d0a27
sigma21 <semihkocakel@hotmail.com>
2021-07-13 15:40:14 +0200

    change main2.py

--- main2.py
+++ main2.py
@@ -1,12 +1,33 @@
+# x = {  'a':37,'b':42,
+
+# 'c':927}
+
+# x = 123456789.123456789E123456789
+
+# if very_long_variable_name is not None and \
+#  very_long_variable_name.field > 0 or \
+#  very_long_variable_name.is_debug:
+#  z = 'hello '+'world'
+# else:
+#  world = 'world'
+ 
+# # fmt: on
+# regular_formatting = [
+#     0,  1,  2,
+#     3,  4,  5,
+#     6,  7,  8,
+# ]
+
 x = {  'a':37,'b':42,
 
 'c':927}
 
 x = 123456789.123456789E123456789
 
+very_long_variable_name=1
+
 if very_long_variable_name is not None and \
- very_long_variable_name.field > 0 or \
- very_long_variable_name.is_debug:
+ very_long_variable_name > 0 :
  z = 'hello '+'world'
 else:
  world = 'world'
