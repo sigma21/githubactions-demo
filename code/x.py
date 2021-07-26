a6dcc216ecc2a85b7ec687451f2ad210713e5b3a
sigma21 <semihkocakel@hotmail.com>
2021-07-26 16:48:14 +0200

    d

---
+++ code/x.py
@@ -0,0 +1,46 @@
+from typing import ClassVar
+
+
+x = {"a": 37, "b": 42, "c": 927}
+
+x = 123456789.123456789e123456789
+
+very_long_variable_name = 1
+
+if very_long_variable_name is not None and very_long_variable_name > 0:
+    z = "hello " + "world"
+else:
+    world = "world"
+
+# fmt: on
+regular_formatting = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+]
+
+
+newvar = 3
+
+D = 1
+
+czvxv = 45
+
+
+safd = 234
+
+
+adfsd = 456456
+
+
+somenewline = 1
+somenewline = 1
+somenewline = 1
+somenewline = 1
+d = 4
